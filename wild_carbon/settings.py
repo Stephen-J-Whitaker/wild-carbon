d9a7592e4b896c87e4685685b88606014f3f6f80
Stephen-J-Whitaker <109547951+Stephen-J-Whitaker@users.noreply.github.com>
2023-06-20 17:55:03 +0000

    FEAT: Update allowed hosts

--- wild_carbon/settings.py
+++ wild_carbon/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wild-carbon-803b1e2f6e91.herokuapp.com']
 
 
 # Application definition
