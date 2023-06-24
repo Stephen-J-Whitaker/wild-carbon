21fbb57035a23a79f286c42434816f548de00b15
Stephen-J-Whitaker <109547951+Stephen-J-Whitaker@users.noreply.github.com>
2023-06-24 17:27:53 +0000

    FEAT: Add robots.txt and sitemap.xml

--- wild_carbon/urls.py
+++ wild_carbon/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    # Code to serve robots.txt sourced at:
+    # https://ngangasn.com/sitemap-robot-txt-django/?expand_article=1
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt',
+                                            content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml',
+                                             content_type='text/xml')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
