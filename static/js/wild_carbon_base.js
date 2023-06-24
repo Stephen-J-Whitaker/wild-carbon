607bfcf5f9556b1a5aea7145a0b41e951a0ddb69
Stephen-J-Whitaker <109547951+Stephen-J-Whitaker@users.noreply.github.com>
2023-06-24 16:55:11 +0000

    FEAT: Add JS to remove hover effect after button click

--- static/js/wild_carbon_base.js
+++ static/js/wild_carbon_base.js
@@ -5,14 +5,26 @@
 /**
  * Add functions for page manipulation on page load completion
  */
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
 
-    //Scroll back to top of page on back to top button click
-    document.getElementById("back-to-top-button").addEventListener("click", function () {
+    // Scroll back to top of page on back to top button click
+    document.getElementById('back-to-top-button').addEventListener('click', function () {
         window.scroll({
             top: 0,
             left: 0,
-            behavior: "smooth",
+            behavior: 'smooth',
         });
     });
+
+    // Remove hover effect from buttons that remain after click
+    document.getElementById('footer-left-back-to-top').addEventListener('click', removeHoverEffect);
+    document.getElementById('footer-right-contact').addEventListener('click', removeHoverEffect);
+    document.getElementById('footer-right-facebook').addEventListener('click', removeHoverEffect);
 });
+
+/**
+ * Remove hover effects from buttons after click
+ */
+function removeHoverEffect() {
+    this.style.border = 'none';
+}
