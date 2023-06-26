31353265d18a00a6e9340793de5da7cf6bac9cf0
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-26 14:06:54 +0000

    FEAT: Register plant model in admin

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Plant
 
-# Register your models here.
+
+class PlantAdmin(admin.ModelAdmin):
+    """
+    Plant admin registration class
+    """
+    list_display = (
+        'genus',
+        'species',
+        'common_name',
+        'image',
+        'price',
+        'sku',
+    )
+
+
+admin.site.register(Plant, PlantAdmin)
