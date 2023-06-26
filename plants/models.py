b6307130789c983b9df888316ca9cf1d1551c89f
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-26 13:58:31 +0000

    FEAT: Add plant model in plants app

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    """
+    Model class to hold plant instances
+    """
+    genus = models.CharField(max_length=150, blank=True)
+    species = models.CharField(max_length=150, blank=True)
+    common_name = models.CharField(max_length=150)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    sku = models.CharField(max_length=150, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        """
+        Override __str__ with the plant common name
+        """
+        return self.common_name
