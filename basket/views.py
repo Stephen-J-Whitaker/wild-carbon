666229c0d0915bd10ee2aa971137855b6614e9a9
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-28 18:06:00 +0000

    FEAT: Add view_basket template and view

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.shortcuts import HttpResponse, get_object_or_404
+from django.contrib import messages
 
-# Create your views here.
+from plants.models import Plant
+
+
+def view_basket(request):
+    """
+    A view that renders the bag contents page
+    view_basket code supplied by Code Institute
+    """
+    return render(request, 'basket/view_basket.html')
