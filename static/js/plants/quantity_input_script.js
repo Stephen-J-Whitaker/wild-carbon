b66ce7fc42cd2a34df7048f82e4cb107f9306fa7
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-28 16:27:37 +0000

    FEAT: Update qty selector js

--- static/js/plants/quantity_input_script.js
+++ static/js/plants/quantity_input_script.js
@@ -12,73 +12,37 @@ document.addEventListener('DOMContentLoaded', function () {
     * which prevents any errors 
     * The following quantity selector code was supplied by Code Institute
     */
-    function handleEnableDisable(itemId, size) {
-        let currentValue;
-        if (size) {
-            currentValue = parseInt($(`.size_${itemId}_${size}`).val());
-        } else {
-            currentValue = parseInt($(`.id_qty_${itemId}`).val());
-        }
+    function handleEnableDisable() {
+
+        let currentValue = parseInt($(`#id-qty`).val());
+        console.log(currentValue)
 
         let minusDisabled = currentValue < 2;
         let plusDisabled = currentValue > 98;
          /* globals $ */
-        if (size) {
-            $(`.decrement-size_${itemId}_${size}`).prop('disabled', minusDisabled);
-            $(`.increment-size_${itemId}_${size}`).prop('disabled', plusDisabled);
-        } else {
-            $(`.decrement-qty_${itemId}`).prop('disabled', minusDisabled);
-            $(`.increment-qty_${itemId}`).prop('disabled', plusDisabled);
-        }
-    }
-
-    // Ensure proper enabling/disabling of all inputs on page load
-    let allQtyInputs = $('.qty_input');
-    for(var i = 0; i < allQtyInputs.length; i++){
-        let itemId = $(allQtyInputs[i]).data('item_id');
-        let size = $(allQtyInputs[i]).data('size');
-        handleEnableDisable(itemId, size);
+        $('#decrement-qty').prop('disabled', minusDisabled);
+        $('#increment-qty').prop('disabled', plusDisabled);
     }
 
     // Check enable/disable every time the input is changed
-    $('.qty_input').change(function() {
-        let itemId = $(this).data('item_id');
-        let size = $(this).data('size');
-        handleEnableDisable(itemId, size);
+    $('#qty-input').change(function() {
+        handleEnableDisable();
     });
 
     // Increment quantity
-    $('.increment-qty').click(function(e) {
-        let allQuantityInputs;
+    $('#increment-qty').click(function(e) {
         e.preventDefault();
-        let itemId = $(this).data('item_id');
-        let size = $(this).data('size');
-        let closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-        if (size) {
-            allQuantityInputs = $(`.input-group-${itemId} input[data-size='${size}']`);
-        } else {
-            allQuantityInputs = $(`.input-group-${itemId} input[name='quantity']`);
-        }
-        let currentValue = parseInt($(closestInput).val());
-        $(allQuantityInputs).val(currentValue + 1);
-        handleEnableDisable(itemId, size);
+        let currentValue = parseInt($('#id-qty').val());
+        $('#id-qty').val(currentValue + 1);
+        handleEnableDisable();
     });
 
     // Decrement quantity
-    $('.decrement-qty').click(function(e) {
-        let allQuantityInputs = 0; 
+    $('#decrement-qty').click(function(e) {
         e.preventDefault();
-        let itemId = $(this).data('item_id');
-        let size = $(this).data('size');
-        let closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-        if (size) {
-            allQuantityInputs = $(`.input-group-${itemId} input[data-size='${size}']`);
-        } else {
-            allQuantityInputs = $(`.input-group-${itemId} input[name='quantity']`);
-        }
-        let currentValue = parseInt($(closestInput).val());
-        $(allQuantityInputs).val(currentValue - 1);
-        handleEnableDisable(itemId, size);
+        let currentValue = parseInt($('#id-qty').val());
+        $('#id-qty').val(currentValue - 1);
+        handleEnableDisable();
     });
     // End of quantity selector code supplied by Code Institute
 });
