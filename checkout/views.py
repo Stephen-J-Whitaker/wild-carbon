3b585a01f39fc27068eccdba4d4d7a55c2126999
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-29 16:47:11 +0000

    FEAT: Add checkout order form

--- checkout/views.py
+++ checkout/views.py
@@ -4,13 +4,13 @@ from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
+# from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 from plants.models import Plant
-from profiles.models import UserProfile
-from profiles.forms import UserProfileForm
-from bag.contexts import basket_contents
+# from profiles.models import UserProfile
+# from profiles.forms import UserProfileForm
+from basket.contexts import basket_contents
 
 import stripe
 import json
