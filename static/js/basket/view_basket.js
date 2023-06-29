a6d493eb88fea3735e621864eb12a20ae0ac2236
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-29 07:54:45 +0000

    FIX: Move quantity form button into form to fix validation

--- static/js/basket/view_basket.js
+++ static/js/basket/view_basket.js
@@ -6,30 +6,18 @@
  * Add functions for page manipulation on page load completion
  */
 document.addEventListener('DOMContentLoaded', function () {
-
     /**
-     * Update quantity on click
+     * Remove item and reload on click
      * Code supplied by Code Institute
      */
-    /* globals $ */
-    $('.update-link').click(function(e) {
-        var form = $(this).prev('.update-form');
-        form.submit();
-    });
-
-      /**
-       * Remove item and reload on click
-       * Code supplied by Code Institute
-       */
-      $('.remove-item').click(function(e) {
-        var csrfToken = "{{ csrf_token }}";
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var url = `/basket/remove_from_basket/${itemId}/`;
-        var data = {'csrfmiddlewaretoken': csrfToken};
+    $('.remove-item').click(function(e) {
+        /* globals $ */
+        let csrfToken = $(this).data('csrf_token');
+        let itemId = $(this).attr('id').split('remove_')[1];
+        let url = `/remove_from_basket/${itemId}/`;
+        let data = {'csrfmiddlewaretoken': csrfToken};
 
         $.post(url, data)
-         .done(function() {
-             location.reload();
-         });
+        .done(function() {location.reload();});
     }); 
 });
