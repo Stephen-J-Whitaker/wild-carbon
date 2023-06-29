0682e4607f52bbda643adf20547b46d357757bd7
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-29 15:02:40 +0000

    FEAT: Update fields in checkout admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,8 +4,9 @@ from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInline(admin.TabularInline):
     """
-    A class to show the order line items in 
+    A class to show the order line items in
     the admin page
+    OrderLineItemAdminInline class provided by Code Institute
     """
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
@@ -14,19 +15,20 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     """
     A class to add orders to the admin page
+    OrderAdmin class provided by Code Institute
     """
     inlines = (OrderLineItemAdminInline,)
 
     readonly_fields = ('order_number', 'date',
                        'vat', 'order_total',
-                       'grand_total', 'original_bag',
+                       'grand_total', 'original_basket',
                        'stripe_pid')
 
     fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'vat',
-              'order_total', 'grand_total', 'original_bag',
+              'order_total', 'grand_total', 'original_basket',
               'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
