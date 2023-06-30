da5f22c07004ede485b26789e02efd6dc7d8cddc
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-30 09:47:03 +0000

    FEAT: Add profile model to admin page

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    Plant admin registration class
+    """
+    list_display = (
+        'user',
+    )
+
+
+admin.site.register(UserProfile, ProfileAdmin)
