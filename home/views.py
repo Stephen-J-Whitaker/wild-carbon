d00d3316a4c8c061503a0ddddfe3230a18a0f066
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-06-30 18:33:17 +0000

    FEAT: Add about us template content, view and url

--- home/views.py
+++ home/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render
 def index(request):
     """
     A view to render the landing page
+    index code provided by Code Institute
     """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """
+    A view to render the about us page
+    """
+    return render(request, 'home/about_us.html')
