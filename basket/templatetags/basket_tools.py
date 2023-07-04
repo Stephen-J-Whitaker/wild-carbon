071de99585bd4a3adcf37fa0a99abf7211129b0b
Stephen-J-Whitaker <steve.j.whitaker@outlook.com>
2023-07-04 18:19:01 +0000

    FEAT: Show plant prices including vat on customer pages

--- basket/templatetags/basket_tools.py
+++ basket/templatetags/basket_tools.py
@@ -1,4 +1,6 @@
 from django import template
+from django.conf import settings
+from decimal import Decimal
 
 
 register = template.Library()
@@ -11,3 +13,12 @@ def calc_subtotal(price, quantity):
     cal_subtotal code supplied by Code Institute
     """
     return price * quantity
+
+
+@register.filter(name='calc_vat')
+def calc_vat(price):
+    """
+    Calculate the vat of a product
+    """
+    plant_vat = price * Decimal(settings.VAT / 100)
+    return plant_vat + price
